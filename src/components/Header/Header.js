4ed8be90f96eaffc184c204417bac9d5e0a94676
afzalhossain00 <afzalhossain.workspace@gmail.com>
2022-10-23 21:39:19 +0600

    update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,8 +11,6 @@ const Header = () => {
                 <NavLink to='/products'>Products</NavLink>
                 <NavLink to='/posts'>Posts</NavLink>
                 <NavLink to='/contacts'>Contacts</NavLink>
-                {/* <a href="/home">Home</a>
-            <a href="/about">About</a> */}
             </nav>
             <h4>Common Header. It won't change.</h4>
         </div>
